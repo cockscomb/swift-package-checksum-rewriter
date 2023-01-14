0ebfa7a21ce7fc176bca8d7bad9af184b40d1c5c
Hiroki Kato <cockscomb@gmail.com>
2023-01-14 10:24:43 +0900

    swift package init

---
+++ Sources/swift-package-checksum-rewriter/swift_package_checksum_rewriter.swift
@@ -0,0 +1,8 @@
+@main
+public struct swift_package_checksum_rewriter {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(swift_package_checksum_rewriter().text)
+    }
+}
---
+++ Tests/swift-package-checksum-rewriterTests/swift_package_checksum_rewriterTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import swift_package_checksum_rewriter
+
+final class swift_package_checksum_rewriterTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(swift_package_checksum_rewriter().text, "Hello, World!")
+    }
+}
